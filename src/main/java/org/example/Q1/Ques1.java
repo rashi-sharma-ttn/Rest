39612dac1f3d322e30c5ecc273648047335ea041
rashi-sharma-ttn <rashi.sharma@tothenew.com>
2023-04-15 18:33:43 +0530

    Commit

--- src/main/java/org/example/Q1/Ques1.java
+++ src/main/java/org/example/Q1/Ques1.java
@@ -1,12 +1,11 @@
 package org.example.Q1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@RestController
 public class Ques1{
-
-    public static void main(String[] args) {
-        SpringApplication.run(Ques1.class, args);
-    }
+    @GetMapping(path = "/welcome")
+    public String getMessage() {
+        return "Hello world!";
+            }
 }
